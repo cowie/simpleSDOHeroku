d24562ba40545fd730ebaeefe791ec32861f0691
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2015-09-28 15:19:46 -0700

    initial push

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 var pg = require('pg');
 var conString = process.env.DATABASE_URL || 'postgres://cdegour:@localhost/sHoHealth';
 var helenID = process.env.HELEN_HIGHRISK_ID || 'badID';
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Welcome!', header:'', success:false, highrisk:false});
+});
+*/
+
+router.get('/', function(req, res, next){
+	res.sendFile(path.join(__dirname + '/../views/index.html'));
+})
+
+router.post('/newsletter', function(req, res, next){
+	//lets just save it into lead
+	pg.connect(conString, function(err, client, done){
+		if(err){
+			console.error(err);res.send('error connecting to db: ' + err);
+		}
+		else{
+			console.log('entering new newsletter request into local db');
+			client.query('INSERT INTO salesforce.lead(firstname, lastname, email) values($1, $2, $3) returning id', 
+				[req.fname, req.lname, req.email],
+				function(err, result){
+					if(err){console.error(err);res.send('error inserting into the table: ' + err + '<br/>');}
+					else{console.log('inserted data, all is well');res.render('thankYou', {});}
+				}
+			);
+		}
+	});
 });
 
 
