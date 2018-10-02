7cf206fe3cc713e4fc6f8752279811ceb8beac25
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2018-10-02 08:08:59 -0700

    first swag once we got the db up, plus removing so much page

--- routes/index.js
+++ routes/index.js
@@ -6,31 +6,48 @@ var pg = require('pg');
 var conString = process.env.DATABASE_URL || 'postgres://cdegour:@localhost/sHoHealth';
 
 router.get('/', function(req, res, next){
-	res.sendFile(path.join(__dirname + '/../views/index.html'));
+  res.sendFile(path.join(__dirname + '/../views/index.html'));
 });
 
 router.get('/thanks', function(req, res, next){
-	res.sendFile(path.join(__dirname+ '/../views/thankYou.html'));
+  res.sendFile(path.join(__dirname+ '/../views/thankYou.html'));
 });
 
+
+router.post('/candidateAdd', function(req, res, next){
+  pg.connect(conString, (err, client, done) => {
+    if(err) {
+      console.error(err);res.send('error connecting to db: ' + err);
+    }
+    else{
+      console.log('entering new candidate');
+      client.query('INSERT INTO "salesforce.candidate__c"("Youtube_Video_ID__c", "Email_Address__c", "First_Name__c", "Last_Name__c", "Twitter_Handle__c" values($1,$2,$3,$4,$5) returning id',
+        [req.body.youtubeID, req.body.email, req.body.fname, req.body.lname, req.body.twitter], function(err, result){
+          if(err){console.error(err);res.send('error inserting into table: ' + err + '<br/>');}
+          else{console.log('inserted, all good'); res.sendFile(path.join(__dirname+'/../views/thankYou.html'));}
+        });
+    }
+  })
+})
+
 router.post('/newsletterAdd', function(req, res, next){
-	//lets just save it into lead
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			console.error(err);res.send('error connecting to db: ' + err);
-		}
-		else{
-			console.log('entering new newsletter request into local db');
-			client.query('INSERT INTO salesforce.lead(firstname, lastname, email) values($1, $2, $3) returning id', 
-				[req.body.fname, req.body.lname, req.body.email],
-				function(err, result){
-					if(err){console.error(err);res.send('error inserting into the table: ' + err + '<br/>');}
-					else{console.log('inserted data, all is well');res.sendFile(path.join(__dirname+ '/../views/thankYou.html'));}
+  //lets just save it into lead
+  pg.connect(conString, function(err, client, done){
+    if(err){
+      console.error(err);res.send('error connecting to db: ' + err);
+    }
+    else{
+      console.log('entering new newsletter request into local db');
+      client.query('INSERT INTO salesforce.lead(firstname, lastname, email) values($1, $2, $3) returning id', 
+        [req.body.fname, req.body.lname, req.body.email],
+        function(err, result){
+          if(err){console.error(err);res.send('error inserting into the table: ' + err + '<br/>');}
+          else{console.log('inserted data, all is well');res.sendFile(path.join(__dirname+ '/../views/thankYou.html'));}
 			
-				}
-			);
-		}
-	});
+        }
+      );
+    }
+  });
 });
 
 
